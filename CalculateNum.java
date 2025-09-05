82f2c11037ce865eef7c55333689c399f74f6c23
gupta3satakshi <159614016+gupta3satakshi@users.noreply.github.com>
2025-09-06 01:43:58 +0530

    Update CalculateNum.java

--- CalculateNum.java
+++ CalculateNum.java
@@ -12,10 +12,24 @@ public class CalculateNum {
 			}
 		}
 	}
+	
+	public static boolean isPrime(int n) {
+		if(n <= 1) 
+			 return false;
+
+			 for(int i =2; i*i<=n; i++) {
+				 if(n % i == 0) {
+					 return false;
+				 }
+			 }
+			 return true;
+		}
 
 	public static void main(String[] args) {
 		List<Integer> num= Arrays.asList(14,25,20,30,27,69,53,11);
 		Predicate<Integer> isPositive = (n) -> n > 0;
+		
+		System.out.println("--------------------Check if a number is a positive number--------------------------");
 		System.out.println(isPositive.test(7));
 		System.out.println(isPositive.test(-7));
 		
@@ -31,19 +45,22 @@ public class CalculateNum {
 				 }
 				 return true;
 		};
+		System.out.println("--------------------Check if a number is prime number by lambda--------------------------");
 		System.out.println(isPrime.test(7));
 		System.out.println(isPrime.test(3));
 		System.out.println(isPrime.test(8));
 		
-		
-
+		System.out.println("--------------------Check if a number is prime number by method reference--------------------------");
+		Predicate isPrimeMethodRef =CalculateNum::isPrime;
+		System.out.println(isPrimeMethodRef.test(31));
+		System.out.println(isPrimeMethodRef.test(12));
+		System.out.println(isPrimeMethodRef.test(95));
 	
-	
-	
+		System.out.println("--------------------print all even numbers--------------------------");
 		operationOnNumber((n)-> n%2 ==0, num);
 		
+		System.out.println("--------------------print all numbers which are divisible by 2,3 and 5--------------------------");
 		operationOnNumber((n)-> n%2 ==0 && n % 3 == 0 && n%5 ==0 , num);
 	}
 }
-
 
